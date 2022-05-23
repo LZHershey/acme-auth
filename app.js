e8bc68027d092c5c5f863e81296102a00e13def9
Havin Mickel <12345678900qw@gmail.com>
2022-05-23 12:25:53 -0700

    Added bcrypt and hased passwords

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const {
-  models: { User },
+  models: { User, Note },
 } = require("./db");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get("/users/:id/notes", async (req, res, next) => {
+  try {
+    const notes = await Note.findAll({
+      where: {
+        userId: req.params.id,
+      },
+    });
+    res.send(notes);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.post("/api/auth", async (req, res, next) => {
   try {
--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const jwt = require("jsonwebtoken");
+const bycrypt = require("bcrypt");
 const config = {
   logging: false,
 };
@@ -18,10 +19,13 @@ const User = conn.define("user", {
   password: STRING,
 });
 
+const Note = conn.define("note", {
+  text: STRING,
+});
+
 User.byToken = async (token) => {
   try {
-    // const user = await User.findByPk(token);
-    const user = await jwt.verify(token, process.env.JWT);
+    const user = await jwt.verify(token, "shh");
     if (user) {
       return user;
     }
@@ -39,19 +43,22 @@ User.authenticate = async ({ username, password }) => {
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
 
-  if (user) {
-    const token = await jwt.sign(user.dataValues, process.env.JWT);
+  if (await bycrypt.compare(password, user.password)) {
+    const token = await jwt.sign(user.dataValues, "shh");
     return token;
-    // return user.id;
   }
   const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
+
+User.beforeCreate(async (user) => {
+  let hashed = await bycrypt.hash(user.password, 5);
+  user.password = hashed;
+});
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
@@ -60,21 +67,47 @@ const syncAndSeed = async () => {
     { username: "moe", password: "moe_pw" },
     { username: "larry", password: "larry_pw" },
   ];
+  const notes = [
+    { text: "Something" },
+    { text: "Another thing" },
+    { text: "List of passwords" },
+    { text: "Wow coding is fun" },
+    { text: "Hello world" },
+  ];
+  const [note1, note2, note3, note4, note5] = await Promise.all(
+    notes.map((note) => Note.create(note))
+  );
+
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
   );
+
+  await lucy.setNotes([note1, note2]);
+  await moe.setNotes([note3, note4]);
+  await larry.setNotes([note5]);
   return {
     users: {
       lucy,
       moe,
       larry,
     },
+    notes: {
+      note1,
+      note2,
+      note3,
+      note4,
+      note5,
+    },
   };
 };
+
+Note.belongsTo(User);
+User.hasMany(Note);
 
 module.exports = {
   syncAndSeed,
   models: {
     User,
+    Note,
   },
 };
