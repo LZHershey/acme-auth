8139941a94773044b30d8a56b2c8502984ed3be2
Lauren Hershey <laurenzhershey@gmail.com>
2022-05-23 13:40:12 -0700

    Completed excercise

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const {
   models: { User, Note },
 } = require("./db");
 const path = require("path");
-const jwt = require("jsonwebtoken");
+
+async function requireToken(req, res, next) {
+  try {
+    const user = await User.byToken(req.headers.authorization);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get("/users/:id/notes", async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
   try {
     const notes = await Note.findAll({
       where: {
         userId: req.params.id,
       },
     });
-    res.send(notes);
+    if (req.user.id === Number(req.params.id)) {
+      res.send(notes);
+    }
   } catch (error) {
     next(error);
   }
@@ -30,9 +41,9 @@ app.post("/api/auth", async (req, res, next) => {
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
